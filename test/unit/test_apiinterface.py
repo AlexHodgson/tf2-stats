68906c4b222c2cae996d02fa58c36f10e8f9e961
Alex Hodgson <56037599+AlexHodgson@users.noreply.github.com>
2020-09-28 22:47:44 +0100

    Update test_apiinterface.py

--- test/unit/test_apiinterface.py
+++ test/unit/test_apiinterface.py
@@ -3,6 +3,7 @@ sys.path.append('A:\Documents\GitHub\\tf2-stats\src')
 
 import unittest
 import api_interface as api # Code to be tested
+import numpy as np
 
 class TestMathMethods(unittest.TestCase):
 
@@ -11,6 +12,13 @@ class TestMathMethods(unittest.TestCase):
 
         test_dict = {'a' : 2, 'b' : 3}
         self.assertEqual(api.hash_dict(test_dict),6**3)
+
+    def test_normalize(self):
+        # Check the normalization function
+
+        test_array = np.random.randint(100, size=(50))
+
+        self.assertEqual(len(test_array), np.sum(api.normalize_rows(test_array)))
 
 if __name__ == '__main__':
     unittest.main()
